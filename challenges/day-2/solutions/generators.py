a2a2feeca45fa5aef973610c598476c12a860d9d
andrew-yarmola <yarmola@math.princeton.edu>
2019-08-13 13:31:06 -0400

    Edits to solutions for day-2

--- challenges/day-2/solutions/generators.py
+++ challenges/day-2/solutions/generators.py
@@ -3,9 +3,8 @@ import random
 def shuffle_gen(start, stop) :
     """ A generator that yields a random permutation
     of the numbers start, start + 1, ..., stop -1. """
-    n = stop - start
     scratch = dict()
-    for remaining in range(n, 0, -1):
+    for remaining in range(stop - start, 0, -1):
         i = random.randrange(remaining)
         # recall that scratch.get(i,i) = scratch[i]
         # if it is defined and scratch.get(i,i) = i otherwise
--- challenges/day-2/solutions/iter_fun.py
+++ challenges/day-2/solutions/iter_fun.py
@@ -1,27 +1,35 @@
 import itertools as it
 
-def count_odds(some_list):
-    return sum([i%2 for i in some_list])
+def count_odds(some_list) :
+    return sum( i % 2 for i in some_list )
 
-def sum_upto_even(some_list):
-    return sum(it.takewhile(lambda x:x%2==1, some_list))
+def count_odds_v2(some_list) :
+    return sum(map(lambda x : x % 2, some_list))
 
-def triangle_num(n):
+def count_odds_v3(some_list) :
+    return len(list(filter(lambda x : x % 2 == 1, some_list)))
+
+def sum_upto_even(some_list) :
+    return sum(it.takewhile(lambda x : x % 2 == 1, some_list))
+
+def triangle_num(n) :
     return list(it.accumulate(range(n)))
 
-def interleave_two(a,b):
-    return list(it.filterfalse(lambda x: x is None, it.chain.from_iterable(it.zip_longest(a,b))))
+def interleave_pair(a,b) :
+    return list(it.chain.from_iterable(zip(a,b)))
 
-def interleave(*data):
-    # input is a sequence of listsx,
-    return list(it.filterfalse(lambda x: x is None, it.chain.from_iterable(it.zip_longest(*data))))
+def interleave(*data) :
+    return list(it.chain.from_iterable(zip(*data)))
 
-def inversion_count(A):
-    return len(list(filter(lambda x:x[0]>x[1],list(it.combinations(A,2)))))
+def interleave_longest(*data) :
+    return list(filter(lambda x : x is not None, it.chain.from_iterable(it.zip_longest(*data))))
 
-def print_groupby(data, group_key):
-    data = sorted(data, key=lambda x: x[group_key])
-    for key, value in it.groupby(data, lambda x: x[group_key]):
+def inversion_count(A) :
+    return sum( A[i] > A[j] for i,j in it.combinations(range(len(A)), 2)) 
+
+def print_groupby(data, group_key) :
+    data = sorted(data, key=lambda x : x[group_key])
+    for key, value in it.groupby(data, lambda x : x[group_key]):
         print(key)
         for item in value:
             print("    ", item)
